2b45500f24821c4054aa7f15d1a8dc3364d83757
emilysupercode <sheil.emily.sc@gmail.com>
2022-11-18 20:43:37 +0100

    1_3 completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,38 @@ class Person {
 
 const person1 = new Person("Eliza", 23)
 person1.info();
+
+// =====================================
+// 1_3
+// =====================================
+
+let firstNameEl = document.querySelector("#firstName");
+let ageEl = document.querySelector("#age");
+let resultText = document.querySelector("#result");
+let passedExam = document.querySelector("#examResult");
+let submitButton = document.querySelector("#submitButton");
+
+class examResults extends Person {
+    constructor(firstName, age, examResult) {
+        super(firstName, age)
+        this.examResult = examResult;
+    }
+}
+
+let newExamResults;
+
+function writeResults() {
+    newExamResults = new examResults(firstNameEl.value, ageEl.value, passedExam.checked)
+    console.log(`${newExamResults.firstName}, ${newExamResults.age} years old, ${newExamResults.examResult}.`)
+    let newLi = document.createElement("li");
+    if (!passedExam.checked) {
+        newLi.style.color = "red";
+    }
+    newLi.innerHTML = `${newExamResults.firstName}, ${newExamResults.age} years old.`
+    resultText.appendChild(newLi);
+}
+
+
+// =====================================
+// 3_15
+// =====================================
