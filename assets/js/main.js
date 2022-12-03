a5861d6a3f224b8d8232f2ad74d401312bad6b73
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-03 15:40:43 +0100

    3_15 added, styling is incomplete

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,61 @@ function writeResults() {
 // =====================================
 // 3_15
 // =====================================
+
+let cardFirstNameEl = document.querySelector("#cardFirstName");
+let cardLastNameEl = document.querySelector("#lastName");
+let cardStageNameEl = document.querySelector("#stageName");
+let cardAlbumsEl = document.querySelector("#albums");
+let cardSinglesEl = document.querySelector("#top10singles");
+let cardNetWorthEl = document.querySelector("#netWorth");
+let cardAgeEl = document.querySelector("#cardAge")
+let cardFamousQuoteEl = document.querySelector("#famousQuote");
+let cardImageURLEl = document.querySelector("#imageURL");
+let inputFormElement = document.querySelector("#inputform");
+
+class Card {
+    constructor(firstName, lastName, stageName, albums, top10singles, netWorth, age, famousQuote, imageURL) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.stageName = stageName;
+        this.albums = albums;
+        this.top10singles = top10singles;
+        this.netWorth = netWorth;
+        this.age = age;
+        this.famousQuote = famousQuote;
+        this.imageURL = imageURL;
+    }
+}
+let outputSection = document.querySelector("#outputSection");
+let cardOutputSection = document.querySelector("#paniniCardOutput");
+let outputName = document.querySelector(".outputName");
+let outputStageName = document.querySelector(".stageName");
+let outputAlbums = document.querySelector(".outputAlbums");
+let outputSingles = document.querySelector(".outputSingles");
+let outputNetWorth = document.querySelector(".outputNetWorth");
+let outputAge = document.querySelector(".outputAge");
+let outputQuote = document.querySelector(".outputQuote");
+
+function createCard(event) {
+    event.preventDefault();
+
+    console.log(cardImageURLEl.value)
+    const newCard = new Card(cardFirstNameEl.value, cardLastNameEl.value, cardStageNameEl.value, cardAlbumsEl.value, cardSinglesEl.value, cardNetWorthEl.value, cardAgeEl.value, cardFamousQuoteEl.value, cardImageURLEl.value)
+
+    console.log(newCard.imageURL)
+
+    inputFormElement.style.display = "none";
+    outputSection.style.display = "block";
+    cardOutputSection.style.backgroundImage = `url(${newCard.imageURL})`;
+    outputName.textContent = `${newCard.firstName} ${newCard.lastName}`;
+    outputStageName.textContent = `${newCard.stageName}`;
+    outputAlbums.textContent = `${newCard.albums}`;
+    outputSingles.textContent = `${newCard.top10singles}`;
+    outputNetWorth.textContent = `${newCard.netWorth} €`;
+    outputAge.textContent = `${newCard.age}`;
+    outputQuote.textContent = `${newCard.famousQuote};`
+}
+
+function resetForm() {
+    document.location.reload();
+}
